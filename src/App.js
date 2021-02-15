bc97347ea91368e8d9b88e708becbedaa6843665
Sam Barker <sam@sambarker.com>
2021-02-15 20:25:43 +0000

    Change text size for title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+        display: 'none',
+        classN: 'spacer',
+    }
+  }
+
+  hideStuff = () => {
+    this.setState({ display: 'flex' })
+    this.setState({ classN: 'spacer-reduced' })
+  }
+
+  render() {
+    return (
+      <div className="main-body">
+        <div className={ this.state.classN }></div>
+        <div className="title">
+          <h1>sambarker.com</h1>
+          <div className="banner">Python Developer | Web Developer</div>
+          <div className="links">
+            <a href="#"><div className="link link-current">Home</div></a>
+            <a href="#"><div className="link" onClick={this.hideStuff}>About</div></a>
+            <a href="#"><div className="link">Portfolio</div></a>
+            <a href="#"><div className="link">Contact</div></a>
+          </div>
+        </div>
+        <div className="main-content" style={{display: this.state.display}}><h3>Hello</h3></div>
+      </div>
+    );
+  }
 }
 
 export default App;
